fbb05613f8cf64e542adb75c16069d7836de6953
NASA <65771915+nodeblackbox@users.noreply.github.com>
2023-04-08 08:43:38 +0100

    Initialized the repository wrong but I'm fixing it
     so far im a little bit impressed

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Rust backend server!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
